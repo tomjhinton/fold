88adb8ea16b481b4cf3c3fb06fc5d6521dfcd50c
Tom Hinton <tomjhinton@gmail.com>
2020-02-06 19:57:41 +0000

    deploy added favicon to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -56,7 +56,8 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: 'src/index.html',
       filename: 'index.html',
-      inject: 'body'
+      inject: 'body',
+      favicon: 'src/assets/favicon.png'
 
     }),
     new webpack.ProvidePlugin({ 'window.decomp': 'poly-decomp' }),
